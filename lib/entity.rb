7285ecdef037569df7945f61bb022da61970a2aa
Jan Deelstra <jan@deelstra.org>
2012-12-24 13:05:00 -0800

    Finished the immutable part

--- lib/entity.rb
+++ lib/entity.rb
@@ -1,18 +1,51 @@
 module Pavlov
   class Entity
-    def self.create &block
-      instance = self.new
-      closure_self = eval "self", block.binding
-      instance.instance_variable_set(:@block_binding_self, closure_self)
-      
-      evaluator = SafeEvaluator.new(instance)
-      evaluator.instance_eval &block
-      
-      instance
+    private_class_method :new
+
+    def self.attributes *args
+      @@attribute_names = []
+      args.each do |attribute_name|
+        define_attribute_setter attribute_name
+        define_attribute_getter attribute_name
+        @@attribute_names << attribute_name
+      end
     end
 
+    def self.create &block
+      update_instance &block
+    end
+
+    def update &block
+      self.class.update_instance self, &block
+    end
+
+    private
     def method_missing(method, *args, &block)
+      # If metdhod isn't found here, look in the closure
+      if @block_binding_self.nil? 
+        super
+      end
       @block_binding_self.send method, *args, &block
+    end
+
+
+    def self.update_instance old_instance = nil, &block
+      new_instance = new
+
+      copy_attribute_values old_instance, new_instance
+
+      safely_evaluate_against(new_instance, &block) if block_given?
+      
+      new_instance
+    end
+
+
+    def self.safely_evaluate_against instance, &block
+      instance.instance_variable_set :@mutable, true
+      instance.instance_variable_set :@block_binding_self, (eval "self", block.binding)
+      evaluator = SafeEvaluator.new(instance)
+      evaluator.instance_eval &block      
+      instance.instance_variable_set :@mutable, false
     end
 
     class SafeEvaluator < BasicObject
@@ -22,6 +55,34 @@ module Pavlov
 
       def method_missing method_name, *args
         @obj.public_send method_name, *args
+      end
+    end
+
+    def self.copy_attribute_values old_instance, new_instance
+      if not old_instance.nil?
+        @@attribute_names.each do |attribute_name|
+          attribute_instance_variable_name = "@#{attribute_name}".to_sym
+          old_value = old_instance.send attribute_name.to_sym
+          new_instance.instance_variable_set attribute_instance_variable_name, old_value
+        end
+      end
+    end
+
+    def self.define_attribute_setter attribute_name
+      define_method attribute_name do
+        attribute_instance_variable_name = "@#{attribute_name}".to_sym
+        instance_variable_get attribute_instance_variable_name
+      end
+    end
+
+    def self.define_attribute_getter attribute_name
+      define_method "#{attribute_name}=" do |new_value|
+        if @mutable
+          attribute_instance_variable_name = "@#{attribute_name}".to_sym
+          instance_variable_set attribute_instance_variable_name, new_value
+        else
+          raise "This entity is immutable, please use 'instance = #{self.class.name}.create do; self.attribute = 'value'; end' or 'instance = instance.update do; self.attribute = 'value'; end'."
+        end
       end
     end
   end
--- tests/entity.rb
+++ tests/entity.rb
@@ -4,7 +4,8 @@ require_relative '../lib/entity'
 class EntityTests < MiniTest::Unit::TestCase
 
   class TestObject < Pavlov::Entity
-    attr_accessor :name, :test
+    attributes :name, :test
+
     private
     def private_method
       puts 'I''m private'
@@ -12,18 +13,14 @@ class EntityTests < MiniTest::Unit::TestCase
   end
 
   def test_returns_not_nil
-    test_object = TestObject.create do
-
-    end
+    test_object = TestObject.create
 
     refute_nil test_object
   end
 
   def test_returns_correct_class
     test_class = TestObject
-    test_object = test_class.create do
-
-    end
+    test_object = test_class.create
 
     assert_equal test_class, test_object.class
   end
@@ -47,10 +44,10 @@ class EntityTests < MiniTest::Unit::TestCase
   end
 
   def helper_method
-    "Ik ben een helper"
+    "I'm a helper method for testing purposes."
   end
 
-  def test_cant_set_private_attribute
+  def test_cant_call_private_methods
     name1 = 'Je naam is mooi.'
     
     assert_raises (NoMethodError) {
@@ -63,11 +60,75 @@ class EntityTests < MiniTest::Unit::TestCase
   def test_can_set_two_attributes
     name1 = 'Je naam is mooi.'
     test_object = TestObject.create do
-      self.name = helper_method
+      self.name = 'Je naam is mooi.'
       self.test = false
     end
 
-    assert_equal helper_method, test_object.name
+    assert_equal name1, test_object.name
     assert_equal false, test_object.test
+  end
+
+  def test_cant_mutate_entity
+    test_object = TestObject.create
+
+    exception = assert_raises (RuntimeError) {
+      test_object.name = 'bla'
+    }
+    assert_equal "This entity is immutable, please use 'instance = EntityTests::TestObject.create do; self.attribute = 'value'; end' or 'instance = instance.update do; self.attribute = 'value'; end'.", 
+      exception.message
+  end
+
+  def test_can_update_entity
+    test_object = TestObject.create
+    other_name = 'Ik heet Pricilla.'
+    
+    test_object = test_object.update do
+      self.name = other_name
+    end
+
+    assert_equal other_name, test_object.name
+  end
+
+  def test_can_partially_update_entity
+    name1 = 'Je naam is mooi.'
+    test_object = TestObject.create do
+      self.test = name1
+    end
+
+    other_name = 'Ik heet Pricilla.'
+    test_object = test_object.update do
+      self.name = other_name
+    end
+
+    assert_equal name1, test_object.test
+    assert_equal other_name, test_object.name
+  end
+
+  def test_returns_other_object_on_update
+    test_object = TestObject.create 
+
+    updated_test_object = test_object.update
+
+    refute_equal test_object.object_id, updated_test_object.object_id
+  end
+
+  def test_cant_call_new
+    exception = assert_raises (NoMethodError) {
+      TestObject.new
+    }
+
+    assert_equal "private method `new' called for EntityTests::TestObject:Class", exception.message
+  end
+
+  def test_can_call_missing_method
+    test_object = TestObject.create
+
+    puts test_object.inspect
+
+    exception = assert_raises (NoMethodError) {
+      test_object.method_is_not_there
+    }
+
+    assert_match /undefined method `method_is_not_there' for #<EntityTests::TestObject.*/, exception.message
   end
 end
