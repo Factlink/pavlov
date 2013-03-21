dce3bc3867fb4f840bb79326bfa86390e1f9ce28
Marten Veldthuis <marten@veldthuis.com>
2013-03-21 13:56:55 +0100

    Cleanup

--- lib/generators/pavlov/interactor_generator.rb
+++ lib/generators/pavlov/interactor_generator.rb
@@ -2,12 +2,10 @@ require 'rails/generators'
 
 module Pavlov
   class InteractorGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../templates', __FILE__)
+
     def create_interactor_file
       template 'interactor.rb', File.join('app/use_cases/interactors', "#{singular_table_name}.rb")
-    end
-
-    def self.source_root
-      File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
     end
   end
 end
--- lib/generators/pavlov/query_generator.rb
+++ lib/generators/pavlov/query_generator.rb
@@ -2,12 +2,16 @@ require 'rails/generators'
 
 module Pavlov
   class QueryGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../templates', __FILE__)
+
     def create_query_file
       template 'query.rb', File.join('app/use_cases/queries', "#{singular_table_name}.rb")
     end
 
-    def self.source_root
-      File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
-    end
+    # This would invoke whatever test framework is defined,
+    # but none seem to have any way of generating for just
+    # any old object instead of "model" or "helper" etc...
+    #
+    # hook_for :test_framework
   end
 end
