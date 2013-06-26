2a6e20a932d234deb2e4da9534163f24612feb99
Mark IJbema <markijbema@gmail.com>
2013-06-26 16:06:06 +0200

    validate should raise when input is invalid
    maybe we want to change this behaviour later, but
    for now the interface should be consistent between
    when we use valid? and validate.

--- lib/pavlov/operation.rb
+++ lib/pavlov/operation.rb
@@ -20,9 +20,9 @@ module Pavlov
 
     def validate
       if respond_to? :valid?
-        valid?
+        raise Pavlov::ValidationError, "an argument is invalid" unless valid?
       else
-        true
+        true # noop
       end
     end
 
