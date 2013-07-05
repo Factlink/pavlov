f61e2d6bdedd15776a44008abda4f6ca5317902c
Tom de Vries <tom@north26.nl>
2013-07-05 14:11:58 +0200

    Add test for Interactor.authenticated?

--- spec/pavlov/interactor_spec.rb
+++ spec/pavlov/interactor_spec.rb
@@ -2,6 +2,47 @@ require_relative '../spec_helper'
 require 'pavlov/interactor'
 
 describe Pavlov::Interactor do
+
+  describe '.authenticated?' do
+    let 'interactor_without_authenticated?' do
+      Class.new do
+        include Pavlov::Interactor
+      end
+    end
+
+    it "raises an error when .authenticated? does not exist" do
+      interactor = interactor_without_authenticated?.new
+
+      expect do
+        interactor.call
+      end.to raise_error(NotImplementedError)
+    end
+
+
+    let 'interactor_with_private_authenticated?' do
+      Class.new do
+        include Pavlov::Interactor
+
+        private
+        def authenticated?
+          false
+        end
+
+        def authorized?
+          true
+        end
+      end
+    end
+
+    it "raises an error when private .authenticated? returns false" do
+      interactor = interactor_with_private_authenticated?.new
+
+      expect do
+        interactor.call
+      end.to raise_error(Pavlov::AccessDenied)
+    end
+  end
+
   let 'interactor_without_authorized?' do
     Class.new do
       include Pavlov::Interactor
