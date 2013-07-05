9ab614e233223224c27728f7ee905d20c2c8ae56
Tom de Vries <tom@north26.nl>
2013-07-05 14:16:15 +0200

    Moved .authorized? test to own describe block

--- spec/pavlov/interactor_spec.rb
+++ spec/pavlov/interactor_spec.rb
@@ -43,40 +43,42 @@ describe Pavlov::Interactor do
     end
   end
 
-  let 'interactor_without_authorized?' do
-    Class.new do
-      include Pavlov::Interactor
-    end
-  end
-
-  it "raises an error when .authorized? does not exist" do
-    interactor = interactor_without_authorized?.new
-
-    expect do
-      interactor.call
-    end.to raise_error(NotImplementedError)
-  end
-
-  let 'interactor_with_private_authorized?' do
-    Class.new do
-      include Pavlov::Interactor
-
-      private
-      def authorized?
-        false
-      end
-
-      def authenticated?
-        true
+  describe '.authorized?' do
+    let 'interactor_without_authorized?' do
+      Class.new do
+        include Pavlov::Interactor
       end
     end
-  end
 
-  it "raises an error when private .authorized? returns false" do
-    interactor = interactor_with_private_authorized?.new
+    it "raises an error when .authorized? does not exist" do
+      interactor = interactor_without_authorized?.new
 
-    expect do
-      interactor.call
-    end.to raise_error(Pavlov::AccessDenied)
+      expect do
+        interactor.call
+      end.to raise_error(NotImplementedError)
+    end
+
+    let 'interactor_with_private_authorized?' do
+      Class.new do
+        include Pavlov::Interactor
+
+        private
+        def authenticated?
+          true
+        end
+
+        def authorized?
+          false
+        end
+      end
+    end
+
+    it "raises an error when private .authorized? returns false" do
+      interactor = interactor_with_private_authorized?.new
+
+      expect do
+        interactor.call
+      end.to raise_error(Pavlov::AccessDenied)
+    end
   end
 end
