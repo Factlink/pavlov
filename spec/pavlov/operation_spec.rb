b2b67b412f7548ef179f763cab6e0d0eb885eeb6
Marten Veldthuis <marten@veldthuis.com>
2013-07-17 11:46:55 +0200

    Move authorization and validation to #call

--- spec/pavlov/operation_spec.rb
+++ spec/pavlov/operation_spec.rb
@@ -9,7 +9,6 @@ describe Pavlov::Operation do
         dummy_class = Class.new do
           include Pavlov::Operation
           arguments :first_argument
-          def authorized?; true; end
         end
 
         operation = dummy_class.new 'argument'
@@ -21,7 +20,6 @@ describe Pavlov::Operation do
         dummy_class = Class.new do
           include Pavlov::Operation
           arguments :var1, :var2
-          def authorized?; true; end
         end
 
         operation = dummy_class.new 'VAR1', 'VAR2'
@@ -29,39 +27,32 @@ describe Pavlov::Operation do
         expect( operation.var1 ).to eq 'VAR1'
         expect( operation.var2 ).to eq 'VAR2'
       end
+    end
+  end
 
-      it "calls validate if it exists" do
-        dummy_class = Class.new do
-          include Pavlov::Operation
-          arguments
-          def validate
-            @x_val = :validate_was_called
-          end
-          def validate_was_called
-            @x_val
-          end
-          def authorized?; true; end
-        end
-        operation = dummy_class.new
-        expect( operation.validate_was_called ).to equal :validate_was_called
+  describe '#call' do
+    let(:dummy_class) do
+      Class.new do
+        include Pavlov::Operation
+        arguments :validation_check, :authorization_check
+        def validate; validation_check.call; end
+        def authorized?; authorization_check.call; end
+        def execute; end
       end
+    end
 
-      it "calls check_authority" do
-        dummy_class = Class.new do
-          include Pavlov::Operation
-          arguments
-          def check_authorization
-            @was_check_authorization_called = :yes
-          end
-          def check_authorization_was_called
-            @was_check_authorization_called
-          end
-          def authorized?; true; end
-        end
-        operation = dummy_class.new
+    let(:validation_check)    { double(call: true) }
+    let(:authorization_check) { double(call: true) }
+    let(:operation) { dummy_class.new(validation_check, authorization_check) }
 
-        expect( operation.check_authorization_was_called ).to equal :yes
-      end
+    it "calls validate if it exists" do
+      operation.call
+      expect(validation_check).to have_received(:call).once
+    end
+
+    it "calls check_authority" do
+      operation.call
+      expect(authorization_check).to have_received(:call).once
     end
   end
 
@@ -76,9 +67,10 @@ describe Pavlov::Operation do
         def authorized?
           true
         end
+        def execute; end
       end
       expect {
-        dummy_class.new 
+        dummy_class.new.call
       }.to_not raise_error
     end
 
@@ -88,9 +80,10 @@ describe Pavlov::Operation do
         def authorized?
           false
         end
+        def execute; end
       end
       expect {
-        dummy_class.new
+        dummy_class.new.call
       }.to raise_error Pavlov::AccessDenied
     end
   end
