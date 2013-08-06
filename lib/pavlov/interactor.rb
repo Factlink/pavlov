4f647bd3377f761d60839d9a33f913838f1d346c
Mark IJbema <markijbema@gmail.com>
2013-08-06 10:42:48 +0200

    Copied ActiveSupport::Concern to our codebase
    This removes the dependency on ActiveSupport, and
    solves a conflict between active support 3.2.14 and
    Virtus, both using InstanceMethods

--- lib/pavlov/interactor.rb
+++ lib/pavlov/interactor.rb
@@ -1,9 +1,9 @@
-require 'active_support/concern'
+require_relative 'concern'
 require 'pavlov/operation'
 
 module Pavlov
   module Interactor
-    extend ActiveSupport::Concern
+    extend Pavlov::Concern
     include Pavlov::Operation
 
     module ClassMethods
