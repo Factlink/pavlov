09518786f37bb742e7cf3b2c448465045f199cec
Marten Veldthuis <marten@veldthuis.com>
2013-09-02 17:50:25 +0200

    Turn backend integration spec into example of how to write isolated tests using backends

--- spec/integration/backend_spec.rb
+++ spec/integration/backend_spec.rb
@@ -4,33 +4,63 @@ require 'pavlov'
 describe 'Pavlov backend' do
   before do
     stub_const 'Interactors', Module.new
-    module Interactors
-      class CreateBlogPost
-        include Pavlov::Interactor
+    class Interactors::CreateBlogPost
+      include Pavlov::Interactor
 
-        attribute :title,     String
-        attribute :published, Boolean, default: true
+      attribute :title,     String
+      attribute :body,      String
+      attribute :published, Boolean, default: true
 
-        private
+      private
 
-        def authorized?
-          pavlov_options[:current_user].is_admin?
-        end
+      def authorized?
+        true
+      end
 
-        def execute
-          title.upcase!
-        end
+      def validate
+      end
+
+      def execute
+        command :create_blog_post, id: available_id,
+                                   title: title,
+                                   body: body,
+                                   published: published
+        Struct.new(:id, :title, :body).new(available_id, title, body)
+      end
+
+      def available_id
+        @available_id ||= query :available_id
       end
     end
 
     stub_const 'Backend', Class.new(Pavlov::Backend)
   end
 
-  let(:current_user) { double(is_admin?: true) }
-  let(:backend)      { Backend.new(pavlov_options: { current_user: current_user }) }
+  let(:current_user)   { double(is_admin?: true) }
+  let(:pavlov_options) { { current_user: current_user } }
+  let(:backend)        { Backend.new(pavlov_options: pavlov_options) }
 
-  it 'calls interactors' do
-    interaction_result = backend.interactor :create_blog_post, title: 'Why you should use Pavlov'
-    expect(interaction_result).to eq 'WHY YOU SHOULD USE PAVLOV'
+  let(:id)    { '123' }
+  let(:title) { 'Why you should use Pavlov' }
+  let(:body)  { 'Some arguments go here' }
+
+  it 'can test interactors based on return values' do
+    backend.stub(:command).with(:create_blog_post, anything)
+    backend.stub(:query).with(:available_id, anything).and_return(id)
+
+    blog_post = backend.interactor :create_blog_post, title: title, body: body
+
+    expect(blog_post.id).to eq(id)
+    expect(blog_post.title).to eq(title)
+    expect(blog_post.body).to eq(body)
+  end
+
+  it 'can test interactors in with mock expectations' do
+    backend.should_receive(:command)
+           .with(:create_blog_post, id: id, title: title, body: body, published: true, pavlov_options: pavlov_options)
+    backend.should_receive(:query)
+           .with(:available_id, pavlov_options: pavlov_options).and_return(id)
+
+    backend.interactor :create_blog_post, title: title, body: body
   end
 end
